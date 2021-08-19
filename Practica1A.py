e477012ed4dfa0995f3408926a3a1959f5de6fde
JR-espacial <jorge_alan_2001@hotmail.com>
2021-08-18 19:33:01 -0500

    practica1

--- Practica1A.py
+++ Practica1A.py
@@ -1,10 +1,9 @@
-
 def coinsToTake(n,values):
     values.sort()
-    totalM = sum(values)
+    mid= sum(values)/2
     money,x = 0,0
     for i in range(len(values)-1, -1, -1):
-        if money <= totalM/2:
+        if money <= mid:
             money += values[i]
             x += 1
         else:
